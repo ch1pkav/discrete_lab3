5fe4c808aa1c6ee9584b2194e8a4221056e6fc8c
Teodor Muzychuk <teodor.muzychuk@ucu.edu.ua>
2022-04-29 23:58:38 +0300

    enabled ascii alphabet

--- crypto.py
+++ crypto.py
@@ -13,19 +13,22 @@ def eratosthenes(num):
     return numbers
 
 
-def generate(a, b):
+def generate(bit_len):
+    b = 2**bit_len - 1
+    a = 2**(bit_len - 1)
     p = randprime(a, b)
     q = randprime(a, b)
     while p == q:
         q = randprime(a, b)
 
-    print(p, q)
+    # print(p, q)
     n = p * q
-    print(n)
-    e = 3
-    print(e)
+    # print(n)
+    e = round((p+q)/8)*2+1
+    # e = 3
     while gcd(e, (p-1) * (q-1)) != 1:
         e += 2
+    # print(e)
 
     d = modInverse(e, (p-1) * (q-1))
 
@@ -64,53 +67,56 @@ def modInverse(a, m):
     return x
 
 
-def find_row_length(n):
-    row = 34
+def find_block_length(n, alphalen):
+    row = alphalen
     i = 0
     while row < n:
-        row *= 100
-        row += 34
+        # print(row)
+        row *= 10**(len(str(alphalen))+1)
+        row += alphalen
         i += 1
-    row //= 100
-    print(row)
+    row //= 10*(len(str(alphalen))+1)
     return i
 
 
 def encrypt(message, n, pubkey):
-    message = [alphabet.find(c) + 1 for c in message]
-    i = find_row_length(n)
-    print(message)
-    print(i)
+    message = [ord(c) for c in message]
+    i = find_block_length(n, 257)
+    # print(message)
+    # print(i)
     message = [message[j:j+i] for j in range(0, len(message), i)]
     while len(message[-1]) < i:
-        message[-1].append(34)
-    print(message)
-    message = [sum(number * 100**index for index, number in
+        message[-1].append(257)
+    # print(message)
+    message = [sum(number * 1000**index for index, number in
                    enumerate(block[::-1])) for block in message]
-    print(message)
+    # print(message)
     message = [pow(block, pubkey, n) for block in message]
-    print(message)
+    # print(message)
     return message
 
 
 def decrypt(message, n, key):
     message = [pow(block, key, n) for block in message]
-    print(message)
+    # print(message)
     decoded_message = []
     for block in message:
         buffer = []
         while block > 0:
-            number = block % 100
-            block //= 100
+            number = block % 1000
+            block //= 1000
             buffer.append(number)
-        print(buffer)
+        # print(buffer)
         decoded_message += buffer[::-1]
-    print(decoded_message)
-    return "".join(alphabet[i-1] if i != 34 else "" for i in decoded_message)
+    # print(decoded_message)
+    return "".join(chr(i) if i != 257 else "" for i in decoded_message)
 
 
 if __name__ == "__main__":
-    keys = generate(100, 10000)
+    keys = generate(1024)
+    # keys = generate(500, 1000)
     print(keys)
-    secret = encrypt("дискретнаматематика", *keys[:-1])
+    message = "lol pryit:)"
+    secret = encrypt(message, *keys[:-1])
+    print(secret)
     print(decrypt(secret, keys[0], keys[2]))
